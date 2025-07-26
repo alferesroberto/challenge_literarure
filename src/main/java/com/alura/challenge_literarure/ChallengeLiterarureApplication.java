b6566f97c0078e69118b267d4b050dd02095f017
ROBERTO ANTONIO ALFERES GOMEZ <alferesroberto77@gmail.com>
2025-07-25 19:37:04 -0600

    inicializando el challenge

---
+++ src/main/java/com/alura/challenge_literarure/ChallengeLiterarureApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challenge_literarure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLiterarureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLiterarureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/challenge_literarure/ChallengeLiterarureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challenge_literarure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiterarureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
